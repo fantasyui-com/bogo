d4dfb61a0616cf2447c9442648f770a43ed84cf9
fantasyui-com <captain@fantasyui.com>
2018-06-19 10:33:06 -0400

    Ported Code and New Build System

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # bogo
-Event Emitter wrapper for Web Sockets
+Event Emitter wrapper for Web Sockets (for use in browsers)
+
+## Install
+
+      npm install bogo
+
+## Usage
+
+```JavaScript
+
+const bogo = require('bogo')(8081);
+
+bogo.on('message', function(data) {
+  console.log('client received: %s', data);
+  bogo.emit('reply', {name:'message', data:'Hello from Client!!!11!!'});
+});
+
+```
---
+++ source.js
@@ -0,0 +1,23 @@
+const EventEmitter = require('events');
+
+module.exports = function({port=8081}){
+
+  // Browser Code
+  const host = window.document.location.host.replace(/:.*/, '');
+  const ws = new WebSocket('ws://' + host + ':'+port);
+
+  // Node Require
+  class Bogo extends EventEmitter {}
+  const bogo = new Bogo();
+
+  // Bogo Core
+  ws.onmessage = function (raw) {
+    const {name, data} = JSON.parse(raw.data);
+    bogo.emit(name, data);
+  };
+
+  bogo.on('reply', (event) => {
+    ws.send(JSON.stringify(event));
+  });
+
+}
